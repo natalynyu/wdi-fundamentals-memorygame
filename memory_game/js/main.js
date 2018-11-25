d65ae37f7e93bd3b7402f958f3538f5c3cfd0824
Natalyn Yu <natalyn.yu@gmail.com>
2018-11-24 21:11:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,5 @@
 console.log("Up and running")
+
 var cards = [
 	{
 	rank: 'queen',
@@ -21,10 +22,35 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 	}
 ];
+
+var myAddEventListener = function (eventType, func) {
+
+}
+
+var createBoard = function (){
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		var eventType = 'click';
+		var superDuperFlipCard = flipCard;
+		cardElement.addEventListener(eventType, flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+	delete flipCard;
+}
+
+
 var cardsInPlay=[];
-var checkForMatch = function(){
-if(cardsInPlay.length===2 && cardsInPlay[0]===cardsInPlay[1]){
-	alert("You found a match!");
+var checkForMatch = function(cardElement){
+	var cardID = cardElement.getAttribute("data-id");
+	cardElement.setAttribute('src', cards[cardID].cardImage);
+	
+	if (cardsInPlay.length===1){
+		;
+	}
+	else if(cardsInPlay.length===2 && cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
 	}
 	else{
 		alert("Sorry, try again.");
@@ -32,13 +58,15 @@ if(cardsInPlay.length===2 && cardsInPlay[0]===cardsInPlay[1]){
 }
 
 
-var flipCard = function(cardID){
+var flipCard = function(){
+	// var this = <<<the element in question >>>
+	var cardElement = this;
+	var cardID = cardElement.getAttribute("data-id"); //The value of this is only not equal to the window when it is being called by the browser upon the click event. 
 	cardsInPlay.push(cards[cardID].rank);
 	console.log("User flipped " + cards[cardID].rank);
 	console.log(cards[cardID].cardImage);
 	console.log(cards[cardID].suit);
-	checkForMatch();
+	checkForMatch(cardElement);
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
